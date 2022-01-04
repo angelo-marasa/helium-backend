c985cce03b51f1c9ba05c908d219546a9cfd2332
angelo-marasa <angelo.marasa@kaleidico.com>
2022-01-04 16:10:41 -0500

    Added vercel to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['POST', 'GET', 'DELETE', 'PUT', '*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://helium-dash.herokuapp.com'],
+    'allowed_origins' => ['http://localhost:3000', 'https://helium-dash.herokuapp.com', 'https://helium-next.vercel.app'],
 
     'allowed_origins_patterns' => ['*'],
 
